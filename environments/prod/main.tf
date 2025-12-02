3d31ed5ebfeb752ba6bdeab9e0c02d1081e1307e
Zhefu Zhang <a723356@gmail.com>
2025-12-02 14:55:24 +0000

    Add network configuration support and update VM IP formats in Terraform files

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,5 +1,6 @@
 module "libvirt" {
   source         = "../../modules/libvirt"
+  network_configs = var.network_configs
   vm_configs     = var.vm_configs
   ssh_config     = var.ssh_config
   rke2_server_ip = var.rke2_server_ip
